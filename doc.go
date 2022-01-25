8df908f7d9c85b443ff38d6cf1f9a53a8285e2fb
xtaci <xtaci@Lis-MacBook-Air.local>
2022-01-25 14:45:58 +0800

    add example

--- doc.go
+++ doc.go
@@ -1,6 +1,50 @@
 // Copyright rockx.com
 // All rights reserved
 
-// This package aims to enhance online staking service procedure
-// with safe usage of master key to derive child keys
+/*
+This package aims to enhance online staking service procedure
+with safe usage of master key to derive child keys
+
+    package main
+
+    import (
+    	"crypto/rand"
+    	"fmt"
+    	"io"
+
+    	"github.com/RockX-SG/eth2deposit"
+    	"github.com/awnumar/memguard"
+    )
+
+    func main() {
+    	// Safely terminate in case of an interrupt signal
+    	memguard.CatchInterrupt()
+    	// Purge the session when we return
+    	defer memguard.Purge()
+
+    	var seed [32]byte
+    	io.ReadFull(rand.Reader, seed[:])
+
+    	// create master key and dervie 100th child key
+    	masterKey := eth2deposit.NewMasterKey(seed)
+    	lockedBuffer, err := masterKey.DeriveChild(100)
+    	if err != nil {
+    		panic(err)
+    	}
+
+    	// create a deposit credential
+    	cred, err := eth2deposit.NewCredential(lockedBuffer, 0, nil, eth2deposit.MainnetSetting)
+    	if err != nil {
+    		panic(err)
+    	}
+
+    	bts, err := cred.MarshalText()
+    	if err != nil {
+    		panic(err)
+    	}
+
+    	fmt.Println(string(bts))
+    }
+
+*/
 package eth2deposit
